19b5972000f58ef63f37d3406d84c1466622d3c6
ivobr <ivobruinsma23@gmail.com>
2025-09-22 10:50:12 +0200

    Factory and Bridge

---
+++ code/Bridge/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        BenzineAuto blueCar = new DAutoBenzine(new Blue());
+        BenzineAuto redCar = new DAutoBenzine(new Red());
+        BenzineAuto greenCar = new DAutoBenzine(new Green());
+        BenzineAuto blankCar = new DAutoBenzine(new Blank());
+        redCar.informatie();
+        System.out.println(" ");
+        greenCar.informatie();
+        System.out.println(" ");
+        blueCar.informatie();
+        System.out.println(" ");
+        blankCar.informatie();
+    }
+}
